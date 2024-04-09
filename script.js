bbeeded9053bbb87f0ce77757fb702c6dcea0548
DenisArchenko <archenko98@mail.ru>
2024-04-09 21:57:58 +0700

    Выполнено первое задание

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
 'use strict'
-var leftborderwidth = 1;
-let second = 2;
-const pi =3.14;
-console.log(leftborderwidth);
-let swsd;
-console.log(swsd);
-let person = {
-    name: 'John',
-    age: 25,
-    isMaried: false
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    svings: false
 };
-console.log(person['name'])
-
-// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-// console.log(arr[0])
-// alert('hELLO world');
-// let an =  confirm('Are you here?');
-// console.log(an);
-// let ans =  prompt('есть вам 18?', 'Да');
-console.log(typeof(arr));
-console.log('arr' + 'dff')
-let inter = 10,
-    dec = 10;
-
-
-console.log(++inter, --dec)
-console.log(5%2);
-
-let listchecked = true,
-    listclouse = false;
-
-console.log(listchecked || listclouse);
-console.log('Pisya')
+let cash = prompt("Введите обязательную статью расходов в этом месяце");
+let cost = prompt("Во сколько это обойдется?");
+let info = alert(money / 30);
+appData.expenses = cost;
+console.log(appData);
