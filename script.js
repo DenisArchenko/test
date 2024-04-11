1c4e6551782984bb05f539f77c17a40707e7cc7f
DenisArchenko <archenko98@mail.ru>
2024-04-11 20:02:50 +0700

    сегодня прошли циклы и сделали 3 задание

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict'
 let money = prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
+
 let appData = {
     budget: money,
     timeData: time,
@@ -9,8 +10,48 @@ let appData = {
     income: [],
     svings: false
 };
-let cash = prompt("Введите обязательную статью расходов в этом месяце");
-let cost = prompt("Во сколько это обойдется?");
-let info = alert(money / 30);
-appData.expenses = cost;
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце");
+    let b = prompt("Во сколько это обойдется?"); 
+
+    if ((typeof(a)) === "string" &&  (typeof(a)) != null && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50)  {
+        console.log('Done!');
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+}
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце"),
+//     b = prompt("Во сколько это обойдется")
+//     if ((typeof(a)) === "string" &&  (typeof(a)) != null && (typeof(b)) != null
+//     && a != '' && b != '' && a.length < 50)  {
+//     i++;
+//     console.log('Done!');
+//     appData.expenses[a] = b;
+//     } else {
+//         i--;
+//     }
+// }
+
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце"),
+//     b = prompt("Во сколько это обойдется")
+//     if ((typeof(a)) === "string" &&  (typeof(a)) != null && (typeof(b)) != null
+//     && a != '' && b != '' && a.length < 50)  {
+//         i++;
+//         console.log('Done!');
+//         appData.expenses[a] = b;
+//     } else {
+//         i--;
+//     }
+// }
+// while (i < 2);
 console.log(appData);
+
+let info = alert("Ваш бюджет на день: " + money / 30);
+
